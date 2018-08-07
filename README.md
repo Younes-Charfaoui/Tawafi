de45487bfe32ab154f88f10be02584287354d327
Younes Charfaoui <mxcsyounes@gmail.com>
2018-08-07 22:41:47 +0100

    Creating The application #1

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# tawaf_app
-An application for the Hojaj to help them in the tawaf.
+# Tawaf
+An application for the Hojaj to help them in the tawaf. by using some math and programming we can make an automatic counter that will count the 7th tours around the kaaba.
---
+++ Tawaf/app/src/main/java/me/mxcsyounes/tawaf/TawafActivity.java
@@ -0,0 +1,13 @@
+package me.mxcsyounes.tawaf;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TawafActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tawaf);
+    }
+}
