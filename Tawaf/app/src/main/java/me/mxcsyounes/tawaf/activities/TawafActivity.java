22a19dc8cd04eac2c0a3e2b53c4bee2beb973ad0
Younes Charfaoui <mxcsyounes@gmail.com>
2018-08-10 00:55:58 +0100

    Adding code to request update of location #6

--- Tawaf/app/src/main/java/me/mxcsyounes/tawaf/activities/TawafActivity.java
+++ Tawaf/app/src/main/java/me/mxcsyounes/tawaf/activities/TawafActivity.java
@@ -1,18 +1,31 @@
 package me.mxcsyounes.tawaf.activities;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
 import me.mxcsyounes.tawaf.R;
 import me.mxcsyounes.tawaf.models.Line2D;
 import me.mxcsyounes.tawaf.models.Point2D;
 
-public class TawafActivity extends AppCompatActivity {
+public class TawafActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     private static final float HIJR_ISMAIL_TOP_LAT = 21.42256366f;
     private static final float HIJR_ISMAIL_TOP_LON = 39.82620367f;
@@ -33,14 +46,24 @@ public class TawafActivity extends AppCompatActivity {
 
     private static final Line2D hajarLine = new Line2D(hijrIsmailBottomPoint, hajarAswadPoint);
     private static final Line2D RoknLine = new Line2D(hijrIsmailTopPoint, roknYamaniPoint);
+    private static final String TAG = "Tawaf";
 
     private int counter = 0;
     private TextView mCounterTextView, mDouaaTextView;
+
+    private GoogleApiClient mGoogleClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tawaf);
+
+        mGoogleClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
 
         mCounterTextView = findViewById(R.id.number_counter_text_view);
         mDouaaTextView = findViewById(R.id.douaa_text_view);
@@ -81,4 +104,50 @@ public class TawafActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        LocationRequest mLocationRequest = LocationRequest.create();
+        mLocationRequest.setInterval(1000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            // public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 55);
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleClient, mLocationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mGoogleClient.disconnect();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.i(TAG, "onLocationChanged: " + location.toString());
+    }
 }
